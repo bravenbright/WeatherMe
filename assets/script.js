665d6a0a4feeb5e9b14d439b3427053832e8ae9b
Kaylon Myers <kaylonmyers2001@gmail.com>
2022-12-06 18:55:50 -0600

    added time button and worked on css

--- assets/script.js
+++ assets/script.js
@@ -3,13 +3,29 @@
 const apiKey = "daae0aee67b04aea9f68cce1499b0a63"
 // document.getElementById('temperature').appendChild(currentTemp);
 // this variable should have the current weather temperature
-// const weatherNow = 
+// const weatherNow =
+ 
+let btnShow = document.querySelector('button');
+let output = document.querySelector('h4');
+
+btnShow.addEventListener('click', () => {
+    let today = new Date();
+
+    let month = today.getMonth() + 1;
+    let year = today.getFullYear();
+    let date = today.getDate();
+
+    let current_date = `${month}/${date}/${year}`
+    output.innerText = current_date;
+
+})
 
 function getApi() { 
     var requestUrl = 'https://api.weatherbit.io/v2.0/current' + '?key=' + apiKey + '&postal_code=76502&country=US';
 
     fetch(requestUrl)
     .then(function(response){
+        
         return response.json()
     })
     .then(function(data) {
